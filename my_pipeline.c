1679c2bee6d27c95b0b33858539f0c46d9093247
Peter Wang <ghw.peter@gmail.com>
2025-11-28 19:43:07 +0000

    New milestone: dump webcam frames
    Webcam -> loopback requires MJPEG to conversion
    Intermediate step: dump frames from webcam

--- my_pipeline.c
+++ my_pipeline.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <fcntl.h> // for open()
 #include <stdio.h>
 #include <sys/ioctl.h> // for ioctl()
@@ -7,7 +8,61 @@
 
 #include "v4l2_helper.h"
 
-// V4L2 demo using integrated webcam, v4l2 loopback and mmap() buffers
+// V4L2 demo using integrated webcam, v4l2 loopback and mmap()
+
+void capture_frames(struct device *capture_device) {
+  if (-1 == ioctl(capture_device->fd, VIDIOC_G_FMT, &capture_device->format)) {
+    errno_exit("VIDIOC_G_FMT");
+  }
+  assert(capture_device->format.type == V4L2_BUF_TYPE_VIDEO_CAPTURE);
+  assert(capture_device->format.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG);
+
+  if (-1 == ioctl(capture_device->fd, VIDIOC_G_FMT, &capture_device->format)) {
+    errno_exit("VIDIOC_G_FMT");
+  }
+
+  mmap_buf(4, capture_device);
+  printf("capture buffers requested\n");
+
+  munmap_buf(capture_device);
+  printf("capture buffers freed\n");
+
+  close(capture_device->fd);
+}
+
+void capture_to_output(struct device *capture_device,
+                       struct device *output_device) {
+  //  Negotiating formats between devices
+  if (-1 == ioctl(capture_device->fd, VIDIOC_G_FMT, &capture_device->format)) {
+    errno_exit("VIDIOC_G_FMT");
+  }
+  output_device->format = capture_device->format;
+  output_device->format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT; // Change type
+  if (-1 == ioctl(output_device->fd, VIDIOC_S_FMT, &output_device->format)) {
+    errno_exit("VIDIOC_S_FMT");
+  }
+
+  // Buffer negotiation -> mmap() -> VIDIOC_STREAMON
+  // 2-4 buffers each for capture and output, at least 2 to stop hardware stalls
+  // Size decided by format negotiation above e.g. 1280/720 'MJPG' ~ 1.2 MB
+
+  mmap_buf(4, capture_device);
+  printf("capture buffers requested\n");
+
+  mmap_buf(4, output_device);
+  printf("output buffers requested\n");
+
+  // Cleanup opposite direction as above
+  // VIDIOC_STREAMOFF -> mumap() -> buffer free -> close device
+  munmap_buf(capture_device);
+  printf("capture buffers freed\n");
+
+  munmap_buf(output_device);
+  printf("output buffers freed\n");
+
+  close(capture_device->fd);
+  close(output_device->fd);
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 3) {
@@ -28,38 +83,7 @@ int main(int argc, char *argv[]) {
     init_err(status);
     return -1;
   }
-
-  //  Negotiating formats between devices
-  if (-1 == ioctl(capture_device.fd, VIDIOC_G_FMT, &capture_device.format)) {
-    errno_exit("VIDIOC_G_FMT");
-  }
-  output_device.format = capture_device.format;
-  output_device.format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT; // Change type
-  if (-1 == ioctl(output_device.fd, VIDIOC_S_FMT, &output_device.format)) {
-    errno_exit("VIDIOC_S_FMT");
-  }
-
-  // Buffer negotiation -> mmap() -> VIDIOC_STREAMON
-  // 2-4 buffers each for capture and output, at least 2 to stop hardware stalls
-  // Size decided by format negotiation above e.g. 1280/720 'MJPG' ~ 1.2 MB
-
-  capture_device.mem_type = V4L2_MEMORY_MMAP; // Placeholder
-  output_device.mem_type = V4L2_MEMORY_MMAP;
-  mmap_buf(4, &capture_device);
-  printf("capture buffers requested\n");
-
-  mmap_buf(4, &output_device);
-  printf("output buffers requested\n");
-
-  // Cleanup opposite direction as above
-  // VIDIOC_STREAMOFF -> mumap() -> buffer free -> close device
-  munmap_buf(&capture_device);
-  printf("capture buffers freed\n");
-
-  munmap_buf(&output_device);
-  printf("output buffers freed\n");
-
-  close(capture_device.fd);
-  close(output_device.fd);
+  capture_frames(&capture_device);
+  // capture_to_output(&capture_device, &output_device);
   return 0;
 }
